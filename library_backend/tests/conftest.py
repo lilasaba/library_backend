b9d35d0dc9becea5b76d137e2edde9d64252aa2c
Lili Szabó <lilasaba@gmail.com>
2020-08-21 13:43:51 +0000

    Add pytests for books_backend

---
+++ library_backend/tests/conftest.py
@@ -0,0 +1,52 @@
+import http.client
+import pytest
+import random
+
+from faker import Faker
+
+from books_backend.app import create_app
+from .constants import PRIVATE_KEY
+
+fake = Faker()
+
+
+@pytest.fixture
+def app():
+    application = create_app()
+
+    application.app_context().push()
+    # Initialise the DB
+    application.db.create_all()
+
+    return application
+
+
+@pytest.fixture
+def book_fixture(client):
+    '''
+    Generate three books in the system.
+    '''
+
+    book_ids = []
+    for _ in range(3):
+        book = {'title': fake.text(100),
+                'book_count': random.randint(1, 10),
+                'year': 2000,
+                'author': fake.text(40),
+                'publisher': fake.text(30)
+                }
+        response = client.post('/admin/add_book/', data=book)
+        assert http.client.CREATED == response.status_code
+        result = response.json
+        book_ids.append(result['id'])
+
+    yield book_ids
+
+    # Delete all books.
+    response = client.get('/admin/books/')
+    books = response.json
+    for book in books:
+        book_id = book['id']
+        url = f'/admin/books/{book_id}/'
+        response = client.delete(url)
+        assert http.client.NO_CONTENT == response.status_code
---
+++ library_backend/tests/test_books.py
@@ -0,0 +1,105 @@
+import http.client
+import random
+
+from faker import Faker
+from unittest.mock import ANY
+
+from .constants import PRIVATE_KEY
+
+fake = Faker()
+
+
+def test_add_book(client):
+    new_book = {'title': fake.text(100),
+                'book_count': random.randint(1, 10),
+                'year': 2000,
+                'author': fake.text(40),
+                'publisher': fake.text(30)
+                }
+    response = client.post('/admin/add_book/', data=new_book)
+    result = response.json
+
+    assert http.client.CREATED == response.status_code
+
+    expected = {'id': ANY,
+                'title': new_book['title'],
+                'book_count': new_book['book_count'],
+                'year': new_book['year'],
+                'time_in': ANY,
+                'author_id': ANY,
+                'publisher_id': ANY,
+    }
+    assert result == expected
+
+
+def test_list_books(client, book_fixture):
+    response = client.get('/admin/books/')
+    result = response.json
+
+    assert http.client.OK == response.status_code
+    assert len(result) > 0
+
+    # Check that the ids are increasing
+    previous_id = -1
+    for book in result:
+        expected = {'id': ANY,
+                    'title': ANY,
+                    'book_count': ANY,
+                    'year': ANY,
+                    'time_in': ANY,
+                    'author_id': ANY,
+                    'publisher_id': ANY
+                    }
+        assert expected == book
+        assert book['id'] > previous_id
+        previous_id = book['id']
+
+
+def test_list_books_search(client, book_fixture):
+    new_book = {'title': 'A tale about a Platypus',
+                'book_count': random.randint(1, 10),
+                'year': 2000,
+                'author': fake.text(40),
+                'publisher': fake.text(30)
+                }
+
+    response = client.post('/admin/add_book/', data=new_book)
+    assert http.client.CREATED == response.status_code
+
+    response = client.get('/admin/books/?search=platypus')
+    result = response.json
+
+    assert http.client.OK == response.status_code
+    assert len(result) > 0
+
+    # Check that the returned values contain "platypus".
+    for book in result:
+        expected = {'id': ANY,
+                    'title': ANY,
+                    'book_count': ANY,
+                    'year': ANY,
+                    'time_in': ANY,
+                    'author_id': ANY,
+                    'publisher_id': ANY
+                    }
+        assert expected == book
+        assert 'platypus' in book['title'].lower()
+
+
+def test_get_book(client, book_fixture):
+    book_id = book_fixture[0]
+    response = client.get(f'/admin/books/{book_id}/')
+    result = response.json
+
+    assert http.client.OK == response.status_code
+    assert 'id' in result
+    assert 'title' in result
+    assert 'year' in result
+    assert 'author_id' in result
+
+
+def test_get_non_existing_book(client, book_fixture):
+    book_id = 123456
+    response = client.get(f'/admin/books/{book_id}/')
+
+    assert http.client.NOT_FOUND == response.status_code
