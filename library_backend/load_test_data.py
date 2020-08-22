da183033a9d7d4ab5c50b7ee61b47ba1d37a2eb3
Lili Szabó <lilasaba@gmail.com>
2020-08-22 08:42:06 +0200

    Fix adding new entries to db for testing

--- library_backend/load_test_data.py
+++ library_backend/load_test_data.py
@@ -13,18 +13,31 @@ if __name__ == '__main__':
 
     # Create test data.
     for i in range(5):
+        author = AuthorModel(name=fake.text(40))
+        publisher = PublisherModel(name=fake.text(40))
+
+        # Create author entry.
+        application.db.session.add(author)
+        application.db.session.flush()
+        application.db.session.refresh(author)
+        author_id = author.id
+        application.db.session.commit()
+
+        # Create publisher entry.
+        application.db.session.add(publisher)
+        application.db.session.flush()
+        application.db.session.refresh(publisher)
+        publisher_id = publisher.id
+        application.db.session.commit()
+
+        # Create book entry.
         book = BookModel(title=fake.text(100),
                          book_count=random.randint(1, 10),
                          year=2000,
-                         author_id=i,
-                         publisher_id=i)
-        author = AuthorModel(id=i, name=fake.text(40))
-        publisher = PublisherModel(id=i, name=fake.text(40))
-
-        application.db.session.add(author)
-        application.db.session.add(publisher)
-        application.db.session.commit()
+                         author_id=author_id,
+                         publisher_id=publisher_id)
         application.db.session.add(book)
+        application.db.session.flush()
         application.db.session.commit()
 
     application.db.session.close()
